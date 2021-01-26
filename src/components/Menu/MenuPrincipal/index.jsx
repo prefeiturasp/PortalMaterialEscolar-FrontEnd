a1e4eba3990b23bf0b00334f4cbc1841122b814b
Kelwy <kelwyduarte@gmail.com>
2021-01-26 18:59:39 -0300

    Adiciona Link do atualização cadastral no portal

--- src/components/Menu/MenuPrincipal/index.jsx
+++ src/components/Menu/MenuPrincipal/index.jsx
@@ -13,7 +13,7 @@ export const MenuPrincipal = () => {
   return (
     <div className="container">
       <div className="row mt-4 mb-4">
-        <div className="col-lg-3 col-sm-12 d-flex justify-content-lg-start justify-content-center align-items-end mb-4 mb-lg-0">
+        <div className="col-lg-2 col-sm-12 d-flex justify-content-lg-start justify-content-center align-items-end mb-4 mb-lg-0">
           <h1 className="m-0">
             <a href="https://educacao.sme.prefeitura.sp.gov.br/">
               <img
@@ -26,7 +26,7 @@ export const MenuPrincipal = () => {
         </div>
         <div
           id="menu-principal"
-          className="col-lg-9 col-sm-12 links-menu d-flex flex-wrap  align-items-end justify-content-lg-end justify-content-start pr-lg-0 mb-xs-4"
+          className="col-lg-10 col-sm-12 links-menu d-flex flex-wrap  align-items-end justify-content-lg-end justify-content-start pr-lg-0 mb-xs-4"
         >
           {window.location.pathname === "/" && (
             <ul className="nav nav-tabs border-0">
@@ -111,6 +111,14 @@ export const MenuPrincipal = () => {
                   className="nav-link text-secondary mb-1 pb-0"
                 >
                   Dúvidas? Veja a lista com perguntas frequentes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href={process.env.REACT_APP_ATUALIZACAO_CADASTRAL_URL}
+                  className="nav-link text-secondary mb-1 pb-0"
+                >
+                  Atualização do cadastro de responsável
                 </a>
               </li>
             </ul>
