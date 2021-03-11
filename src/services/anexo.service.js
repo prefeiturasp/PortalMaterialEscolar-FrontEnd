c2c32dbae72e0b51cd62fd151067c596f39c127e
anderson89marques <andersonoanjo18@gmail.com>
2021-03-11 16:18:30 -0300

    Consultas de anexos na area logada passando o Token JWT.

--- src/services/anexo.service.js
+++ src/services/anexo.service.js
@@ -1,13 +1,21 @@
 import { API_URL } from "../config";
+import { getToken } from "./auth.service";
 
 export const setAnexo = async (payload) => {
   const url = `${API_URL}/anexos/`;
   let status = 0;
-  return fetch(url, {
-    headers: {
+  let header = {
       "Accept-Language": "pt-br",
       "Content-Type": "application/json",
-    },
+  }
+
+  const token = getToken();
+  if (token !== undefined) {
+    header["Authorization"] = `JWT ${token}`;
+  }
+  
+  return fetch(url, {
+    headers: header,
     method: "POST",
     body: JSON.stringify(payload),
   })
@@ -26,11 +34,18 @@ export const setAnexo = async (payload) => {
 export const deleteAnexo = async (uuid) => {
   const url = `${API_URL}/anexos/${uuid}/`;
   let status = 0;
-  return fetch(url, {
-    headers: {
+  let header = {
       "Accept-Language": "pt-br",
       "Content-Type": "application/json",
-    },
+  }
+
+  const token = getToken();
+  if (token !== undefined) {
+    header["Authorization"] = `JWT ${token}`;
+  }
+
+  return fetch(url, {
+    headers: header,
     method: "DELETE",
   })
     .then((response) => {
@@ -48,14 +63,22 @@ export const deleteAnexo = async (uuid) => {
 export const setFachadaLoja = async (payload, uuid) => {
   const url = `${API_URL}/lojas/${uuid}/`;
   let status = 0;
-  return fetch(url, {
-    headers: {
+  
+  let header = {
       "Accept-Language": "pt-br",
       "Content-Type": "application/json",
-    },
+  }
+
+  const token = getToken();
+  if (token !== undefined) {
+    header["Authorization"] = `JWT ${token}`;
+  }
+
+  return fetch(url, {
+    headers: header,
     method: "PATCH",
     body: JSON.stringify(payload),
-  })
+  }) 
     .then((res) => {
       status = res.status;
       return res.json();
